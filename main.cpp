1cc9efc6f90bd9ea9806c96f0e4d0239e87c753b
teresabat <60197944+teresabat@users.noreply.github.com>
2024-06-23 17:46:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,33 @@
 
 using namespace std;
 int main(){
-cout<<"Hello, world!";
+
+double n1, n2;
+char op;
+
+cout<<"Digite seu primeiro numero: ";
+cin>>n1;
+
+cout<<"Digite a operação: ";
+cin>>op;
+
+cout<<"Digite o segundo numero: ";
+cin>>n2;
+
+switch(op){
+  case'+':
+    cout<<(n1+n2)
+  break;
+  case '-':
+    cout<<(n1-n2)
+  break;
+  case'*':
+    cout<<(n1*n2)
+  break;
+  case'/':
+      cout<<(n1/n2)
+  break;
+    default:
+      return 0;
+  }
 }
